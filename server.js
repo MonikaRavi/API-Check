cb189fdc6c53e387bb30dff67bce93995403e895
biplavtimalsina <btimalsina@nevada.unr.edu>
2018-07-26 14:23:51 -0700

    First Commit of Server

---
+++ server.js
@@ -0,0 +1,35 @@
+const express=require("express");
+const hbs=require("hbs");
+var app=express();
+
+app.set('view engine','hbs');
+app.use(express.static(__dirname +'/public'));
+
+app.get("/",function(req,res){
+	//res.send('<h1>Hello Express!</h1>');
+	res.send({
+		name:'Andrew',
+		likes:[
+		'biking',
+		'cities'
+		]
+	});
+});
+
+
+app.get("/about",function(req,res){
+	res.render("about",{
+		pageTitle:'About Page1',
+		currentYear:new Date().getFullYear()
+	});
+});
+
+app.get("/bad",function(req,res){
+	res.send({
+		name:'Error Message'
+	});
+});
+
+app.listen(3000,function(){
+	console.log("Server starting..");
+});
