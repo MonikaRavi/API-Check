73b126f917b68bc788ce438ff4fff8a848a2f33d
Monika Ravi <monikar@hawsco.com>
2018-07-31 15:58:11 -0700

    Error Handling

--- server.js
+++ server.js
@@ -95,7 +95,7 @@ app.get("/",function(req,res){
 	// 	'cities'
 	// 	]
 	// });
-	res.send('<h1>Welcome to Haws Drinking Fountain API!!! </h1> <br><br> <br> <br> <br>  Copyright @Hawsco');
+	res.send("Welcome to Haws Fountain API!!! <br><br> <br> <br> <br>  Copyright @Hawsco");
 });
 
 
@@ -151,85 +151,104 @@ app.get("/bad",function(req,res){
 // });
 app.get("/rfid/:rfid",function(req,res){
 	var reqRfid=req.params.rfid;
-	//console.log(reqRfid);
-	var allmatchingLog=[];
-	var totalConsumption=0;
-	var totalLogs=0;
-	Object.keys(data2["MPRlog"]).forEach(function(key){
-		//console.log("Key= "+key);
-		//console.log("data2.collection.key.rfid: "+ data2["MPRlog"][key]["rfid"]);
-		var tempRfid=data2["MPRlog"][key]["rfid"];
-		if(tempRfid===reqRfid)
-		{
-			allmatchingLog.push(data2["MPRlog"][key]);
-			//totalConsumption+=parseInt(data2["MPRlog"][key]["flow"]);
-			var tempVal= (Number(data2["MPRlog"][key]["flow"]));
-			//console.log(tempVal);
-			//console.log(typeof(tempVal));
-			totalConsumption=totalConsumption+tempVal;
-			//console.log('partial consumption:' + totalConsumption);
-			 totalLogs=Number(totalLogs)+1;
-		}
-	});
-	//console.log(allmatchingLog);
-	//console.log( totalConsumption);
-	var logData={
-		'totalLogs':totalLogs,
-		'totalConsumption':totalConsumption
-	};
+	var regexRfid=RegExp('([a-zA-Z0-9]{8}$)');
+	console.log(reqRfid.length);
+	console.log("regexRfid.test(reqRfid)=" + regexRfid.test(reqRfid));
+	if(regexRfid.test(reqRfid)==true && reqRfid.length===8){
+		//console.log(reqRfid);
+		var allmatchingLog=[];
+		var totalConsumption=0;
+		var totalLogs=0;
+		Object.keys(data2["MPRlog"]).forEach(function(key){
+			//console.log("Key= "+key);
+			//console.log("data2.collection.key.rfid: "+ data2["MPRlog"][key]["rfid"]);
+			var tempRfid=data2["MPRlog"][key]["rfid"];
+			if(tempRfid===reqRfid)
+			{
+				allmatchingLog.push(data2["MPRlog"][key]);
+				//totalConsumption+=parseInt(data2["MPRlog"][key]["flow"]);
+				var tempVal= (Number(data2["MPRlog"][key]["flow"]));
+				//console.log(tempVal);
+				//console.log(typeof(tempVal));
+				totalConsumption=totalConsumption+tempVal;
+				//console.log('partial consumption:' + totalConsumption);
+				 totalLogs=Number(totalLogs)+1;
+			}
+		});
+		//console.log(allmatchingLog);
+		//console.log( totalConsumption);
+		var logData={
+			'totalLogs':totalLogs,
+			'totalConsumption':totalConsumption
+		};
 
-	allmatchingLog.push(logData);
-	//console.log(allmatchingLog);
-	res.send(allmatchingLog);
+		allmatchingLog.push(logData);
+		//console.log(allmatchingLog);
+		res.send(allmatchingLog);
+	}else{
+		res.send("Invalid Rfid tag. Please re-check your rfid. It must have 8 alphanumeric characters!!!");
+	}
+
+	
+	
 });
 
 
 app.get("/:emailId",function(req,res){
 	var reqEmailId=req.params.emailId;
-
+	var regexEmail=RegExp('([a-z0-9][-a-z0-9_\+\.]*[a-z0-9])@([a-z0-9][-a-z0-9\.]*[a-z0-9]\.(arpa|root|aero|biz|cat|com|coop|edu|gov|info|int|jobs|mil|mobi|museum|name|net|org|pro|tel|travel|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cu|cv|cx|cy|cz|de|dj|dk|dm|do|dz|ec|ee|eg|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|sk|sl|sm|sn|so|sr|st|su|sv|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|um|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)|([0-9]{1,3}\.{3}[0-9]{1,3}))');
+	//console.log(regexEmail.test("biplav.timalsina@gmail.com"));
 	//console.log("reqEmail= "+reqEmailId);
-	var matchingLog=[];
-	// console.log("matching log= "+matchingLog);
-	// console.log("__________________________________________");
-	Object.keys(data).forEach(function(key){
-		//console.log("key= "+key);
-		//console.log("key.email= "+ data[key].email);
-		// console.log("type of email:"+ typeof(data[key].email));
-		// console.log("type of req:"+typeof(reqEmailId));
-		if(data[key].email===reqEmailId){
-			matchingLog.push(data[key]);
-		}
-	});
-	//console.log("matching log= ");
-	//console.log(matchingLog);
-	var reqRfid=matchingLog[0].rfid;
-//	console.log("matching rfid: "+reqRfid);
-	//console.log(data["2740efeb"]);
-	//var data=[1,2,3];
-//	console.log("__________________________________________");
-	//console.log(JSON.stringify(res.body));
-	var allMatchingLog=[];
+	if(regexEmail.test(reqEmailId)){
+		var matchingLog=[];
+		//console.log("matching log= "+matchingLog);
+		//console.log("__________________________________________");
+		Object.keys(data).forEach(function(key){
+			//console.log("key= "+key);
+			//console.log("key.email= "+ data[key].email);
+			// console.log("type of email:"+ typeof(data[key].email));
+			// console.log("type of req:"+typeof(reqEmailId));
+			if(data[key].email===reqEmailId){
+				matchingLog.push(data[key]);
+			}
+		});
+		//console.log("matching log= ");
+		//console.log(matchingLog);
+		var reqRfid=matchingLog[0].rfid;
+		//console.log("matching rfid: "+reqRfid);
+		//console.log(data["2740efeb"]);
+		//var data=[1,2,3];
+		//console.log("__________________________________________");
+		//console.log(JSON.stringify(res.body));
+		var allMatchingLog=[];
 
-	//extract all the transaction of this reqRfid
-	//console.log(data2["MPRLog"]["-LFZjowOUbZszA9x7YFH"]["filterExpired"]);
-//	console.log(data2["MPRlog"]["-LFZjowOUbZszA9x7YFH"]);
-	//var collection="MPRlog";
-	//console.log(data2.collection["-LFZjowOUbZszA9x7YFH"]);
-	Object.keys(data2["MPRlog"]).forEach(function(key){
-		//console.log("Key= "+key);
-		//console.log("data2.collection.key.rfid: "+ data2["MPRlog"][key]["rfid"]);
-		var tempRfid=data2["MPRlog"][key]["rfid"];
-		if(tempRfid===reqRfid)
-		{
-			allMatchingLog.push(data2["MPRlog"][key]);
-		}
+		//extract all the transaction of this reqRfid
+		//console.log(data2["MPRLog"]["-LFZjowOUbZszA9x7YFH"]["filterExpired"]);
+		//console.log(data2["MPRlog"]["-LFZjowOUbZszA9x7YFH"]);
+		//var collection="MPRlog";
+		//console.log(data2.collection["-LFZjowOUbZszA9x7YFH"]);
+		Object.keys(data2["MPRlog"]).forEach(function(key){
+			//console.log("Key= "+key);
+			//console.log("data2.collection.key.rfid: "+ data2["MPRlog"][key]["rfid"]);
+			var tempRfid=data2["MPRlog"][key]["rfid"];
+			if(tempRfid===reqRfid)
+			{
+				allMatchingLog.push(data2["MPRlog"][key]);
+			}
 
-	});
-	//console.log(allMatchingLog);
-	res.send(allMatchingLog);
-	//res.render("emailId",{data:data["2740efeb"]});
+		});
+		//console.log(allMatchingLog);
+		res.send(allMatchingLog);
+		//res.render("emailId",{data:data["2740efeb"]});
+	}else{
+		res.send("Invalid Email address");
+	}
+	
 });
 
+app.get("*",function(req,res){
+	res.send("You are trying to reach an end point which doesn't exist!!! Please try again");
+});
 /*
 app.listen(3000,function(){
 	console.log("Server starting..");
